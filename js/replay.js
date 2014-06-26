5b0153ac6534a1ad8c93827be76034701b09ff1a
Evelyn Eastmond <evhan55@gmail.com>
2014-06-25 21:52:24 -0400

    marking progress at http://plomaproject.tumblr.com/post/89917256380/one-is-the-original-scan-one-is-the-ploma-render

--- js/replay.js
+++ js/replay.js
@@ -11,7 +11,7 @@ var pat2;
 var pat3;
 var paperColor = "rgb(253, 253, 240)";
 var paperColor = "rgb(255, 255, 253)";
-var paperColor = "rgb(254, 255, 219)";
+var paperColor = "rgb(255, 254, 219)";
 //var paperColor = "rgb(255, 255, 255)";
 var capture;
 
