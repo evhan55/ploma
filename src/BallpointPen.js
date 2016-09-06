b18d8890477cb09cb1ab0a2fa31dff7659b7d8fb
athomschke <astrid.thomschke@gmail.com>
2016-09-06 08:21:21 +0200

    move library comment into main file

--- src/BallpointPen.js
+++ src/BallpointPen.js
@@ -4,19 +4,6 @@ import { paperColorDark, penR, penG, penB, filterWeight, filterWeightInverse, st
 import {Texture} from './Texture';
 import Point from './Point';
 
-/*
-Ploma - High-fidelity ballpoint pen rendering for tablets with pressure-sensitive styluses
-v0.4
-
-Evelyn Eastmond
-Dan Amelang
-Viewpoints Research Institute
-
-(c) 2014-2015
-
-TODO: License
-*/
-
 'use strict'; // for strict mode
 
 //////////////////////////////////////////////
--- src/ploma.js
+++ src/ploma.js
@@ -1,1 +1,14 @@
+/*
+Ploma - High-fidelity ballpoint pen rendering for tablets with pressure-sensitive styluses
+v0.4
+
+Evelyn Eastmond
+Dan Amelang
+Viewpoints Research Institute
+
+(c) 2014-2015
+
+TODO: License
+*/
+
 export { BallpointPen } from './BallpointPen';
